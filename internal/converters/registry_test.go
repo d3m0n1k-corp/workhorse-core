769c1a70f9cc64e2950ad943b6f9dc5436e65dc2
Gagandeep Singh Bhambrah <grimmshax@gmail.com>
2025-03-16 00:29:56 +0530

    feat: :sparkles: Update converter registration to use reflect.Type for config and enhance error handling in NewConverter (#4)

--- internal/converters/registry_test.go
+++ internal/converters/registry_test.go
@@ -1,10 +1,34 @@
 package converters
 
 import (
+	"encoding/json"
+	"fmt"
+	"reflect"
 	"testing"
 
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
+
+type MockConfig struct {
+	mock.Mock
+}
+
+func (m *MockConfig) Validate() error {
+	m.Called()
+	return nil
+}
+
+type TestConfig struct {
+	Action string
+}
+
+func (t *TestConfig) Validate() error {
+	if t.Action != "success" {
+		return fmt.Errorf("Invalid config")
+	}
+	return nil
+}
 
 func TestRegister_whenNewRegistration_returnNil(t *testing.T) {
 	reg := Registration{
@@ -36,4 +60,58 @@ func TestGetRegistration_whenFound_returnRegistration(t *testing.T) {
 func TestGetRegistration_whenNotFound_returnNil(t *testing.T) {
 	_, err := GetRegistration("test")
 	require.NotNil(t, err)
+}
+
+func TestNewConverter_whenRegistrationNotFound_returnError(t *testing.T) {
+	_, err := NewConverter("test", "{}")
+	require.Error(t, err)
+}
+
+func TestNewConverter_whenInvalidConfigJson_returnError(t *testing.T) {
+	reg := Registration{
+		Name:   "test",
+		Config: reflect.TypeOf(MockConfig{}),
+	}
+	registry[reg.Name] = reg
+	_, err := NewConverter(reg.Name, "{")
+	require.Error(t, err)
+	delete(registry, reg.Name)
+}
+
+func TestNewConverter_whenInvalidConfigType_returnError(t *testing.T) {
+	reg := Registration{
+		Name:   "test",
+		Config: reflect.TypeOf(struct{ Name string }{}),
+	}
+	registry[reg.Name] = reg
+	_, err := NewConverter(reg.Name, "{}")
+	require.Error(t, err, "Invalid config type")
+	delete(registry, reg.Name)
+}
+
+func TestNewConverter_whenInvalidConfigValues_returnError(t *testing.T) {
+	reg := Registration{
+		Name:   "test",
+		Config: reflect.TypeOf(TestConfig{}),
+	}
+	registry[reg.Name] = reg
+	_, err := NewConverter(reg.Name, "{}")
+	require.Error(t, err, "Invalid config")
+	delete(registry, reg.Name)
+}
+
+func TestNewConverter_whenValidConfig_returnConverter(t *testing.T) {
+	reg := Registration{
+		Name:   "test",
+		Config: reflect.TypeOf(TestConfig{}),
+		Constructor: func(config BaseConfig) BaseConverter {
+			return nil
+		},
+	}
+	registry[reg.Name] = reg
+	config := TestConfig{Action: "success"}
+	configStr, _ := json.Marshal(config)
+	_, err := NewConverter(reg.Name, string(configStr))
+	require.NoError(t, err)
+	delete(registry, reg.Name)
 }
