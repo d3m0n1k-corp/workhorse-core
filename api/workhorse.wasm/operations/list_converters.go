e3a94db049a5b72cbb36218b02d52163d1452b96
Gagandeep Singh Bhambrah <grimmshax@gmail.com>
2025-03-25 01:15:15 +0530

    feat: ✨ Added Logging functionality (#9)
    * feat: :sparkles: Add converter execution functionality and update converter listing to return structured data
    
    * feat: :sparkles: Add Codecov configuration for coverage reporting and thresholds
    
    * feat: :sparkles: Refactor WASM API to implement logging and operations for converter execution and listing
    
    * refactor: :recycle: Rename list_connectors function to list_converters for consistency in API
    
    * feat: :sparkles: Refactor WASM API to implement logging and operations for converter execution and listing
    
    * refactor: :recycle: Rename list_connectors function to list_converters for consistency in API

---
+++ api/workhorse.wasm/operations/list_converters.go
@@ -0,0 +1,24 @@
+//go:build js && wasm
+
+package operations
+
+import (
+	"encoding/json"
+	"syscall/js"
+	"workhorse-core/api/workhorse.wasm/common"
+	"workhorse-core/app"
+)
+
+func List_converters(this js.Value, args []js.Value) any {
+	connList := app.ListConverters()
+	// l.GlobalStream.Logf("Found %d connectors", len(connList))
+	response_object := common.Response{
+		Result: connList,
+		Error:  nil,
+	}
+	resp_json, err := json.Marshal(response_object)
+	if err != nil {
+		panic(err)
+	}
+	return js.ValueOf(string(resp_json))
+}
